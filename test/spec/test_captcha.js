8b6fc7192bd771b7846d90589bbe394b5507403b
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2016-10-29 18:17:02 +0200

    Make failing test pass

--- test/spec/test_captcha.js
+++ test/spec/test_captcha.js
@@ -2,6 +2,7 @@ describe("The constructor is supposed a proper Captcha object", function() {
     it('Constructor Captcha exists', function(){
         expect(Captcha).toBeDefined();
     });
+    var captcha = new Captcha();
     it("Captcha object is not null", function(){
        expect(captcha).not.toBeNull();
     });
