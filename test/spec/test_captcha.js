d915a20733462e619234116bed5bc51ee121f60c
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2016-10-29 18:16:34 +0200

    Add test to chekc if captcha object is null. The test fails

--- test/spec/test_captcha.js
+++ test/spec/test_captcha.js
@@ -2,4 +2,7 @@ describe("The constructor is supposed a proper Captcha object", function() {
     it('Constructor Captcha exists', function(){
         expect(Captcha).toBeDefined();
     });
+    it("Captcha object is not null", function(){
+       expect(captcha).not.toBeNull();
+    });
 });
