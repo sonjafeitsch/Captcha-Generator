ccdb86e55c3dbfb673344e141bc068b83056c782
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2016-10-29 18:19:08 +0200

    Add test if captcha is instance of Captcha class

--- test/spec/test_captcha.js
+++ test/spec/test_captcha.js
@@ -6,4 +6,7 @@ describe("The constructor is supposed a proper Captcha object", function() {
     it("Captcha object is not null", function(){
        expect(captcha).not.toBeNull();
     });
+    it('captcha object should be an instance of Captcha class', function(){
+        expect(captcha instanceof Captcha).toBeTruthy();
+    });
 });
