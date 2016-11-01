a3dd4c32dd5a8f5cdf9b7b54b1b8cea588235760
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2016-11-01 10:59:47 +0100

    Implement function and make one failing test pass

--- js/captchaGenerator.js
+++ js/captchaGenerator.js
@@ -9,38 +9,51 @@
 function Captcha() {
     this.height = 100;
     this.width = 300;
-    this.fingerprintLenght = 4;
+    this.fingerprintLength = 4;
+    this.fingerprintHash = "";
 }
 
 /**
- *
+ * Functions which generates a random fingerprint and return a svg
  * @returns {string|*|string}
  */
-Captcha.prototype.getPNG = function(){
-    var fingerprint = [];
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789?#$%&";
+Captcha.prototype.getSVG = function(){
+    var that = this;
+    var fingerprint = "";
+    var possible = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz023456789?#$%&";
 
-    for( var i=0; i < 4; i++ ){
-        fingerprint.push(possible.charAt(Math.floor(Math.random() * possible.length)));
+    for( var i=0; i < that.fingerprintLength; i++ ){
+        fingerprint += possible.charAt(Math.floor(Math.random() * possible.length))
     }
-    console.log(fingerprint);
+    that.fingerprintHash = CryptoJS.MD5(fingerprint);
 
-    // var svg = "<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>" +
-    //     "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve'>" +
-    //     "<text x='100 40 70 100' y='100' style='font-size:55; font-family:Times New Roman, Courier; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1'transform='rotate(20, 100, 100)'>" +
-    //     fingerprint[0] +
-    //     "</text><text x='170 40 70 100' y='100' style='font-size:60; font-family:Arial black, Arial; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(-40, 170, 100)'>" +
-    // fingerprint[1] +
-    // "</text" +
-    // "><text x='200 40 70 100' y='100' style='font-size:35; font-family:Comic Sans MS, Arial; font-weight: bold; " +
-    // "font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(30, 200, 100)'>" +
-    //     fingerprint[2] +"</text>" +
-    //     "<text x='250 40 70 100' y='100' style='font-size:37; font-family:Courier new, Courier; font-weight: bold; " +
-    //     "font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(-20, 250, 100)'>" +
-    //     fingerprint[3] +"</text>" +
-    //     "<filter id='filter1' filerUnits='objectBoundingBox' x='0%' width='100%' height='100%'><feTurbulence type='turbulence' baseFrequency='0.03' numOctaves='5' />" +
-    //     "</filter><rect x='100' y='50' width='250' height='100' filter='url(#filter1)' />" +
-    //     "<line x1='100' y1='50' x2='290' y2='50' /><line x1='100' y1='120' x2='290' y2='120' /><line x1='100' y1='50' x2='100' y2='120' /><line x1='290' y1='50' x2='290' y2='120' />g>";
+    var svg = "<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>" +
+        "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve'>" +
+        "<text x='100 40 70 100' y='100' style='font-size:55; font-family:Times New Roman, Courier; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1'transform='rotate(20, 100, 100)'>" +
+        fingerprint[0] +
+        "</text><text x='170 40 70 100' y='100' style='font-size:60; font-family:Arial black, Arial; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(-40, 170, 100)'>" +
+    fingerprint[1] +
+    "</text" +
+    "><text x='200 40 70 100' y='100' style='font-size:35; font-family:Comic Sans MS, Arial; font-weight: bold; " +
+    "font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(30, 200, 100)'>" +
+        fingerprint[2] +"</text>" +
+        "<text x='250 40 70 100' y='100' style='font-size:37; font-family:Courier new, Courier; font-weight: bold; " +
+        "font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(-20, 250, 100)'>" +
+        fingerprint[3] +"</text>" +
+        "<filter id='filter1' filerUnits='objectBoundingBox' x='0%' width='100%' height='100%'><feTurbulence type='turbulence' baseFrequency='0.03' numOctaves='5' />" +
+        "</filter><rect x='100' y='50' width='250' height='100' filter='url(#filter1)' />" +
+        "<line x1='100' y1='50' x2='290' y2='50' /><line x1='100' y1='120' x2='290' y2='120' /><line x1='100' y1='50' x2='100' y2='120' /><line x1='290' y1='50' x2='290' y2='120' />g>";
 
     return svg;
+};
+
+/**
+ * Function which check if the user input is equal to the fingerprint
+ * @param input
+ * @returns {boolean}
+ */
+Captcha.prototype.checkCaptcha = function(input){
+    var that = this;
+    var inputAsHash = CryptoJS.MD5(input);
+    return inputAsHash.toString() === that.fingerprintHash.toString();
 };
