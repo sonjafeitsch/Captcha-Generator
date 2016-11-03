c1325e4e44c81454e7ab66e7fbe6c99ab57b867f
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2016-11-03 12:39:29 +0100

    Change style of captcha to a default one

--- js/captchaGenerator.js
+++ js/captchaGenerator.js
@@ -29,20 +29,20 @@ Captcha.prototype.getSVG = function(){
 
     var svg = "<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>" +
         "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve'>" +
-        "<text x='100 40 70 100' y='100' style='font-size:55; font-family:Times New Roman, Courier; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1'transform='rotate(20, 100, 100)'>" +
+        "<rect x='0' y='50' width='250' height='100' fill='#ef8216' z-index='-1' />" +
+        "<line x1='100' y1='50' x2='290' y2='50' /><line x1='100' y1='120' x2='290' y2='120' /><line x1='100' y1='50' x2='100' y2='120' /><line x1='290' y1='50' x2='290' y2='120' />g>" +
+        "<text x='20 40 70 100' y='110' style='font-size:40px; font-family:Times New Roman, Courier; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1'transform='rotate(20, 100, 100)'>" +
         fingerprint[0] +
-        "</text><text x='170 40 70 100' y='100' style='font-size:60; font-family:Arial black, Arial; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(-40, 170, 100)'>" +
+        "</text>" +
+        "<text x='80 40 70 100' y='120' style='font-size:35px; font-family:Arial black, Arial; font-weight: bold; font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(-5, 170, 100)'>" +
     fingerprint[1] +
     "</text" +
-    "><text x='200 40 70 100' y='100' style='font-size:35; font-family:Comic Sans MS, Arial; font-weight: bold; " +
+    "><text x='130 20 10 10' y='110' style='font-size:35px; font-family:Comic Sans MS, Arial; font-weight: bold; " +
     "font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(30, 200, 100)'>" +
         fingerprint[2] +"</text>" +
-        "<text x='250 40 70 100' y='100' style='font-size:37; font-family:Courier new, Courier; font-weight: bold; " +
-        "font-style=oblique; stroke: black; stoke-width:1; fill:none'transform='rotate(-20, 250, 100)'>" +
-        fingerprint[3] +"</text>" +
-        "<filter id='filter1' filerUnits='objectBoundingBox' x='0%' width='100%' height='100%'><feTurbulence type='turbulence' baseFrequency='0.03' numOctaves='5' />" +
-        "</filter><rect x='100' y='50' width='250' height='100' filter='url(#filter1)' />" +
-        "<line x1='100' y1='50' x2='290' y2='50' /><line x1='100' y1='120' x2='290' y2='120' /><line x1='100' y1='50' x2='100' y2='120' /><line x1='290' y1='50' x2='290' y2='120' />g>";
+        "<text x='200 40 70 100' y='100' style='font-size:37px; font-family:Courier new, Courier; font-weight: bold; " +
+        "font-style=oblique; stroke: black; stoke-width:50; fill:none'transform='rotate(-20, 250, 100)'>" +
+        fingerprint[3] +"</text>";
 
     return svg;
 };
